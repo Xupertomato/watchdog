3bd1dbda53adb79c6127396d1c510f5104f0d752
Xupertomato <cilovwx@gmail.com>
2023-03-04 14:55:54 +0800

    Second

--- watchdog/settings.py
+++ watchdog/settings.py
@@ -132,8 +132,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# Optional: you can also add the following line to tell Django where to collect static files
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
